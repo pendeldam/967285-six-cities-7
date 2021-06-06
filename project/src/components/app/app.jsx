40f91392809015312429a3bfade156662f663728
Алексей Сергеев <pendelladam@gmail.com>
2021-06-07 00:29:50 +0300

    added components: 1. main-page; 2. offer-card;

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import MainPage from '../main-page/main-page';
 
-function App() {
-  return <p>Hello, world!</p>;
+function App({offersCount}) {
+  return <MainPage offersCount={offersCount}/>;
 }
+
+App.propTypes = {
+  offersCount: PropTypes.number.isRequired,
+};
 
 export default App;
--- project/src/index.js
+++ project/src/index.js
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const Setting = {
+  offersCount: 312,
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App offersCount={Setting.offersCount}/>
   </React.StrictMode>,
   document.getElementById('root'));
