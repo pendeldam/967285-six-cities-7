870c580691cb817bc2b5d95095508a232bbcbefc
Алексей Сергеев <pendelladam@gmail.com>
2021-06-10 22:44:02 +0300

    1. added city-places component; 2. replaced state with active offer from offer-list to city-places;

--- project/src/components/offer-list/offer-list.jsx
+++ project/src/components/offer-list/offer-list.jsx
@@ -1,20 +1,18 @@
-import React, {useState} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import OfferCard from '../offer-card/offer-card';
-import offerProps from '../offer-card/offer-card.prop';
+import stylesProp from '../offer-card/styles.prop';
 
-function OfferList({offers}) {
-  const [activeOffer, setActiveOffer] = useState(null);
-
+function OfferList(props) {
   return (
-    <div className="cities__places-list places__list tabs__content">
-      {offers.map((offer) => <OfferCard offer={offer} key={offer.id} setActiveOffer={setActiveOffer}/>)}
+    <div className={props.styles.main}>
+      {props.render()}
     </div>
   );
 }
 
 OfferList.propTypes = {
-  offers: PropTypes.arrayOf(offerProps).isRequired,
+  styles: stylesProp.isRequired,
+  render: PropTypes.func.isRequired,
 };
 
 export default OfferList;
