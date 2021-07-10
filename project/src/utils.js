2bb0a77ee02e2f1e0ca35129f9f68f2e1d5e195d
Алексей Сергеев <pendelladam@gmail.com>
2021-07-10 20:35:27 +0300

    removed mocks

--- project/src/utils.js
+++ project/src/utils.js
@@ -2,14 +2,6 @@ import {AuthorizationStatus} from './const';
 
 export const formatDate = (date) => new Date(date).toLocaleDateString('en-US', {month: 'long', year: 'numeric'});
 
-export const getRandomIntegerNumber = (min, max) =>(Math.floor(Math.random() * (max - min) + min));
-
-export const getRandomArrayItem = (array) => array[getRandomIntegerNumber(0, array.length)];
-
-export const getRandomArray = (length, cb) => new Array(length)
-  .fill('')
-  .map(cb);
-
 export const isCheckedAuth = (authStatus) => authStatus === AuthorizationStatus.UNKNOWN;
 
 export const isWrongPassword = (value) => [...value].every((it) => it === ' ');
